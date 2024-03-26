dcc4eb1f582c00e53425de7f115d48a66c45edc9
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-03-26 22:22:59 +0200

    Update index.js (delete endpoint)

--- index.js
+++ index.js
@@ -66,5 +66,20 @@ app.post('/api/events', (req, res) => {
     res.send(event)
 });
 
+
+// DELETE API endpoint
+app.delete('/api/events/:id', (req, res) => {
+  const eventId = parseInt(req.params.id);
+  const eventIndex = events.findIndex(c => c.id === eventId);
+
+  if (eventIndex === -1) {
+    res.status(404).send(`Event with ID ${eventId} not found!`);
+  } else {
+    events.splice(eventIndex, 1); // Remove event from array
+    res.status(200).send(`Event with ID ${eventId} deleted successfully!`);
+  }
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`listening on port ${port} ...`));
