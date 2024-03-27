c6e08068fe998e4e3dd8d2cccf7c41a79069e6e5
yasmenattia <yasmin180951@feng.bu.edu.eg>
2024-03-27 02:28:03 +0200

    route to put event

--- routes/event_router.js
+++ routes/event_router.js
@@ -4,6 +4,7 @@ const eventrouter = Router();
 const eventcontroller = require('../controllers/event_controller')
 
 eventrouter.post('/',eventcontroller.addEvent);
+eventrouter.put('/api/events/:id',eventController.editEventById);
 
 
 module.exports = {
