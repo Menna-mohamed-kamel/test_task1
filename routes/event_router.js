01715385e02d8d54248a9916fe95b0aa8b41ca2f
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-03-27 04:20:41 +0200

    Update event_router.js

--- routes/event_router.js
+++ routes/event_router.js
@@ -6,6 +6,7 @@ const eventcontroller = require('../controllers/event_controller');
 eventRouter.get('/', eventController.getAllEvents);
 eventrouter.post('/',eventcontroller.addEvent);
 eventrouter.put('/api/events/:id',eventController.editEventById);
+eventRouter.delete('/:id', eventController.deleteEvent);
 
 
 module.exports = {
