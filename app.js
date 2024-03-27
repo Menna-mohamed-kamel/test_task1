84299f3020d746aae1f559616a1ce1524eacf90d
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-03-27 04:28:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { eventrouter } = require('./routes/event_router');
+const { eventrouter } = require('./routes/event_router') ;
 
 app.use('/api/events/', eventrouter);
 
