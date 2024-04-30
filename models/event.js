351131266cad9f81571c9d68a6d3a76b6e947723
Mai-Adel1 <mai196079@feng.bu.edu.eg>
2024-04-30 23:21:24 +0300

    improve code interface

--- models/event.js
+++ models/event.js
@@ -1,8 +1,3 @@
-// const events = [
-//     { id: 1, name: 'Event 1', date: '2024-04-01', location: 'Venue 1' },
-//     { id: 2, name: 'Event 2', date: '2024-04-15', location: 'Venue 2' },
-//     { id: 3, name: 'Event 3', date: '2024-04-30', location: 'Venue 3' }
-
 const { default: mongoose } = require("mongoose");
 const eventSchema = new mongoose.Schema({
     name:{
