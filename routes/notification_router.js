94bf2cab4c8c6fe7a0df0a31f721caa588db3c5c
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-05-02 05:37:00 +0300

    Update notification_router.js

--- routes/notification_router.js
+++ routes/notification_router.js
@@ -1,1 +1,14 @@
+const { Router } = require('express');
+const notificationRouter = Router();
 
+const notificationController = require('../controllers/notification_controller');
+
+notificationRouter.get('/', notificationController.getAllNotifications);
+notificationRouter.get('/:id', notificationController.getNotificationById);
+notificationRouter.post('/', notificationController.createNotification);
+notificationRouter.put('/:id', notificationController.updateNotificationById);
+notificationRouter.delete('/:id', notificationController.deleteNotificationById);
+
+module.exports = {
+    notificationRouter,
+};
